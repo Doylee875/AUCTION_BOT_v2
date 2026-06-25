79c4ed55ba7c364c4528721bdaf6d46a11ad07ce
Doylee875 <85373846+Doylee875@users.noreply.github.com>
2026-06-25 21:49:14 +0300

    Revert "Update main.py"
    This reverts commit 4da576284da4d7a8b0b0c1058a6bd680a79d8a5b.

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def main() -> None:
         action="store_true",
         help="Пропустить синхронизацию БД",
     )
-    args = parser.parse_known_args()[0]
+    args = parser.parse_args()
 
     # Настройка логирования
     api.utils.logger.setup_logging(
